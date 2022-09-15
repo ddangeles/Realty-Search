bc6d8d2049a33c2928836fd96743882b7db5bd58
donnadangeles@gmail.com <donnadangeles@gmail.com>
2022-09-15 11:17:35 -0700

    Pokemon integrations

--- js/script.js
+++ js/script.js
@@ -27,6 +27,17 @@
                 var imgEl = document.createElement("img");
                 var priceEl = document.createElement("p");
                 var floorPlanEl = document.createElement("p");
+                var pokemonEl = document.createElement("div");
+                if (response.data.listings[i].price_raw <500000) {
+                    pokemonEl.setAttribute("class", "animate__animated animate__jello squirtleSprite")
+                    pokemonEl.style.setProperty('--animate-duration', '5s')
+                } else if (response.data.listings[i].price_raw >500000 && response.data.listings[i].price_raw <1000000) {
+                    pokemonEl.setAttribute("class", "animate__animated animate__jello wartortleSprite")
+                    pokemonEl.style.setProperty('--animate-duration', '5s')
+                } else if (response.data.listings[i].price_raw >1000000) {
+                    pokemonEl.setAttribute("class", "animate__animated animate__jello blastoiseSprite")
+                    pokemonEl.style.setProperty('--animate-duration', '5s')
+                }
                 devEl.textContent = response.data.listings[i].address;
                 imgEl.src = response.data.listings[i].photo;
                 priceEl.textContent = "Price " + response.data.listings[i].price;
@@ -43,43 +54,52 @@
                 address.append(imgEl);
                 address.append(devEl);
                 address.append(priceEl);
+                address.append(pokemonEl);
                 address.append(floorPlanEl);
             
             }
-
+            getPokes()
            }).catch(function (error) {
             console.error(error);
         });
 
+async function getPokes() {
+try {
+    const poke1 = await axios.get('https://pokeapi.co/api/v2/pokemon/squirtle')
+    const poke2 = await axios.get('https://pokeapi.co/api/v2/pokemon/wartortle')
+    const poke3 = await axios.get('https://pokeapi.co/api/v2/pokemon/blastoise')
+    console.log(poke1, poke2, poke3)
+    console.log(poke1.data.sprites.front_shiny)
+    var squirtleDivs = document.getElementsByClassName("squirtleSprite")
+    console.log(squirtleDivs) 
+    for(var index = 0; index < squirtleDivs.length; index++) {
+        var squirtleImage = document.createElement("img")
+        squirtleImage.src = poke1.data.sprites.front_shiny
+        squirtleDivs[index].appendChild(squirtleImage)
+    }
 
-    // axios.get('https://pokeapi.co/api/v2/pokemon/squirtle')
-    // .then(res => {
-    //     console.log(res)
-    //     console.log(res.data.abilities[0].ability.name)
-    // })
-    // .catch(err => [
-    //     console.log(err)
-    // ])
+    var wartortleDivs = document.getElementsByClassName("wartortleSprite")
+    console.log(wartortleDivs) 
+    for(var index = 0; index < wartortleDivs.length; index++) {
+        var wartortleImage = document.createElement("img")
+        wartortleImage.src = poke2.data.sprites.front_shiny
+        wartortleDivs[index].appendChild(wartortleImage)
+    }
 
-    // axios.get('https://pokeapi.co/api/v2/pokemon/wartortle')
-    // .then(res => {
-    //     console.log(res)
-    //     console.log(res.data.abilities[0].ability.name)
-    // })
-    // .catch(err => [
-    //     console.log(err)
-    // ])
+    var blastoiseDivs = document.getElementsByClassName("blastoiseSprite")
+    console.log(blastoiseDivs) 
+    for(var index = 0; index < blastoiseDivs.length; index++) {
+        var blastoiseImage = document.createElement("img")
+        blastoiseImage.src = poke3.data.sprites.front_shiny
+        blastoiseDivs[index].appendChild(blastoiseImage)
+    }
+}
+catch (err) {
+    console.log(err)
+}
+}
 
-    // axios.get('https://pokeapi.co/api/v2/pokemon/blastoise')
-    // .then(res => {
-    //     console.log(res)
-    //     console.log(res.data.abilities[0].ability.name)
-    // })
-    // .catch(err => [
-    //     console.log(err)
-    // ])
 
-    
 
     // Pokemon in use: Blastoise => Wartortle => Squirtle
 
