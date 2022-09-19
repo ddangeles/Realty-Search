82a53052b8fd3475aec56fa9b025d1e31568a136
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-09-19 09:57:47 -0700

    branch update

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ function getAddresses(limit) {
             column.setAttribute("class","col s4")
             var devEl       = document.createElement("div");
             devEl.setAttribute("class","card");
-            devEl.style.height="550px";
+            devEl.style.height="580px";
             devEl.style.width="462px";
             var imgDiv      = document.createElement("div");
             imgDiv.setAttribute("class","card-image");
@@ -102,17 +102,17 @@ function getAddresses(limit) {
 
             if (response.data.listings[i].price_raw < 500000) {
 
-                pokemonEl.setAttribute("class", "animate__animated animate__shakeY squirtleSprite")
+                pokemonEl.setAttribute("class", "animate__animated animate__shakeX squirtleSprite centered")
                 pokemonEl.style.setProperty('--animate-duration', '5s')
 
             } else if (response.data.listings[i].price_raw > 500000 && response.data.listings[i].price_raw <1000000) {
 
-                pokemonEl.setAttribute("class", "animate__animated animate__shakeY wartortleSprite")
+                pokemonEl.setAttribute("class", "animate__animated animate__shakeX wartortleSprite centered")
                 pokemonEl.style.setProperty('--animate-duration', '5s')
 
             } else if (response.data.listings[i].price_raw > 1000000) {
 
-                pokemonEl.setAttribute("class", "animate__animated animate__shakeY blastoiseSprite")
+                pokemonEl.setAttribute("class", "animate__animated animate__shakeX blastoiseSprite centered")
                 pokemonEl.style.setProperty('--animate-duration', '5s')
 
             }
@@ -134,6 +134,7 @@ function getAddresses(limit) {
                             var mapURL = result.data[0].map_url;
                             console.log(i+" : " + mapURL);
                             document.getElementById(i+"").setAttribute("href",mapURL);
+                            document.getElementById(i+"").setAttribute("target","_blank");
                             
                         })
                          
@@ -148,17 +149,18 @@ function getAddresses(limit) {
             } else {
 
                 imgEl.src = response.data.listings[i].photo;
-                
 
             }
             imgEl.style.height= "310px";
-            imgEl.style.width="460px"
+            imgEl.style.width="460px";
             // imgEl.src = response.data.listings[i].photo;
 
             priceEl.textContent = "Price " + response.data.listings[i].price;
 
             var hasBedrooms     = response.data.listings[i].beds;
             var hasBathrooms    = response.data.listings[i].baths;
+            var sqft            = response.data.listings[i].sqft;
+            console.log("sqft : "+ sqft)
             var floorPlanString = "";
 
             if (hasBedrooms === 0) {
@@ -170,13 +172,17 @@ function getAddresses(limit) {
                 floorPlanString = hasBedrooms + " Bedrooms - " + hasBathrooms + " Bath";
             
             }
-            
+
+            var pricePerSqft = (response.data.listings[i].price_raw/response.data.listings[i].sqft_raw).toFixed(2);
+            var sqftEl = document.createElement("p");
+            sqftEl.textContent= `${sqft}, price/sqft: $${pricePerSqft}`;
             floorPlanEl.textContent = floorPlanString;
             
             imgDiv.append(imgEl);
             devEl.append(cardAddress);
             devEl.append(imgDiv);
             cardContent.append(priceEl);
+            cardContent.append(sqftEl);
             cardContent.append(floorPlanEl);
             cardContent.append(pokemonEl);
             cardContent.append(urlEl);
@@ -225,6 +231,7 @@ function getAddresses(limit) {
                 }
                 var squirtleImage = document.createElement("img")
                 squirtleImage.src = poke1.data.sprites.front_shiny
+                squirtleImage.setAttribute("class","centered")
 
                 squirtleDivs[index].appendChild(squirtleImage)
 
@@ -241,6 +248,7 @@ function getAddresses(limit) {
                 
                 var wartortleImage = document.createElement("img")
                 wartortleImage.src = poke2.data.sprites.front_shiny
+                wartortleImage.setAttribute("class","centered");
 
                 wartortleDivs[index].appendChild(wartortleImage)
 
@@ -256,7 +264,7 @@ function getAddresses(limit) {
                 }
                 var blastoiseImage = document.createElement("img")
                 blastoiseImage.src = poke3.data.sprites.front_shiny
-
+                blastoiseImage.setAttribute("class","centered");
                 blastoiseDivs[index].appendChild(blastoiseImage)
 
             }
